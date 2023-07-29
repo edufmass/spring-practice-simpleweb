ce32fa8f3593e230be956777c95701712db3a88a
edufmass <git@edufmass.ar>
2023-07-29 02:58:42 -0300

    Override entities toString, equals and hashCode

--- src/main/java/ar/net/edufmass/springwebapp/domain/Book.java
+++ src/main/java/ar/net/edufmass/springwebapp/domain/Book.java
@@ -71,4 +71,28 @@ public class Book {
         this.authors = authors;
     }
 
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
 }
--- src/main/java/ar/net/edufmass/springwebapp/domain/Publisher.java
+++ src/main/java/ar/net/edufmass/springwebapp/domain/Publisher.java
@@ -89,4 +89,31 @@ public class Publisher {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addressLine1='" + addressLine1 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id == publisher.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
